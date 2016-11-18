147a1b0de0fe79a3f8e47aa2602133baf9d3b289
Kevin Hoffman <kehoffman3@gmail.com>
2016-11-18 15:40:16 -0500

    Setup storyboard and home view controller with table view.

--- Lantearn/Lantearn/Views/HomeViewController.swift
+++ Lantearn/Lantearn/Views/HomeViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var viewModel: HomeViewModel = HomeViewModel(expenseDataSource: [Expense](), incomeDataSource: [Income]())
+    
+    //MARK: View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        title = "Lantearn"
+        
+        setupTableView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Setup
 
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+    }
+    
+    //MARK: UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.numberOfSections()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection(section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = viewModel.rowTextForIndexPath(indexPath)
+        
+        return cell
+    }
+    
+    //MARK: UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.headerTextForSection(section)
+    }
+    
 }
 
