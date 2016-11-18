02c8f890d5bca0dd812aba97695fec37a7dfb5fa
Kevin Hoffman <kehoffman3@gmail.com>
2016-11-18 14:00:23 -0500

    Init project

---
+++ Lantearn/Lantearn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lantearn
+//
+//  Created by Kevin Hoffman on 11/18/16.
+//  Copyright © 2016 kehoffman3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
